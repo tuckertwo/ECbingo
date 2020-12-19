12eaa978b8956df6d617fe12150e4ab7bc111f9b
Tucker R. Twomey <tucker@the-twomeys.com>
2020-12-18 22:24:56 -0800

    Implement the main part of the Verhoeff algorithm

--- src/Verhoeff.jl
+++ src/Verhoeff.jl
@@ -43,6 +43,9 @@ verhoeff_onestep(t::Int, (p, x)) = verhoeff_onestep((p, x), t)
 @test verhoeff_onestep((2, 3), 3) ==  1
 @test verhoeff_onestep((3, 2), 1) ==  2
 
+verhoeff_alg(xs::Array) = foldl(verhoeff_onestep, collect(enumerate(xs)), init=0)
+verhoeff_alg(x::Int)     = verhoeff_alg(digits(x))
+
 # verhoeff_verify verifies a number with a Verhoeff algorithm check digit
 verhoeff_verify(x)          = verhoeff_alg(x) == 0
 @test verhoeff_verify(2363)
