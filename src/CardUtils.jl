08ae9f18465841f8a6812c1ea45a9d3c77a4017b
Tucker R. Twomey <tucker@the-twomeys.com>
2021-01-01 02:54:15 -0800

    Write function for PostScript to PDF conversion

--- src/CardUtils.jl
+++ src/CardUtils.jl
@@ -1,3 +1,14 @@
 module CardUtils
 
+export ps2pdf
+
+# Converts PostScript files to PDF files.
+# Requires GhostScript to be installed and the `ps2pdf` utility to be in the
+# program's $PATH.
+ps2pdf(ps) = open(`ps2pdf - -`, read=true, write=true) do
+  write(proc.in, ps)
+  close(proc.in)
+  read(proc.out, String) 
+end
+
 end
