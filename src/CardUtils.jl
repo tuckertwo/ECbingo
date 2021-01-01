9a4206e90dbe4e81c2de6ea62a523cc418b93c8e
Tucker R. Twomey <tucker@the-twomeys.com>
2021-01-01 04:01:14 -0800

    Write function to create proper matrix of card numbers

--- src/CardUtils.jl
+++ src/CardUtils.jl
@@ -1,5 +1,7 @@
 module CardUtils
 
+using StatsBase
+
 export ps2pdf
 
 # Converts PostScript files to PDF files.
@@ -10,5 +12,12 @@ ps2pdf(ps) = open(`ps2pdf - -`, read=true, write=true) do proc
   close(proc.in)
   read(proc.out, String) 
 end
+
+# TODO: Write tests
+gen_rand_col(rng, yl, replace) = (r) -> sample(rng, r, yl, replace=replace)
+
+# TODO: Write tests
+gen_rand_array(rng; ranges=[01:10, 11:20, 21:30, 31:40, 41:50], yl=5,
+               replace=false) = hcat(map(gen_rand_col(rng, yl, replace), ranges)...)
 
 end
