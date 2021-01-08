94139d5247b71664bf948075a97cd45be10dfbc0
Tucker R. Twomey <tucker@the-twomeys.com>
2021-01-08 12:21:35 -0800

    Add rudimentary card-generation functionality to webapp

---
+++ app/resources/cardgen/CardgenController.jl
@@ -0,0 +1,28 @@
+module CardgenController
+using Genie, Genie.Router, Genie.Requests, Genie.Renderer
+using Cards, CardUtils, Verhoeff
+using Random
+
+const round_colors = ["maroon", "navy", "DarkMagenta"]
+
+autoinc = let i=-1; () -> i+=1 end
+numgen = autoinc()
+
+wrap_contenttype(data, type) = respond(WebRenderable(
+    body=data,
+    status=200,
+    headers=Dict("Content-Type" => type)))
+
+gencard_ps(id, round) = mk_card(gen_rand_array(MersenneTwister(id)),
+  round_colors[round], num=id, round=round)
+
+gencard_pdf = ps2pdf ∘ gencard_ps
+
+gencard_ps_r() = wrap_contenttype(gencard_ps(payload(:id), payload(:round)),
+                                  "application/postscript")
+
+gencard_pdf_r() = wrap_contenttype(gencard_pdf(payload(:id), payload(:round)),
+                                   "application/pdf")
+
+genseq(sym) = () -> redirect(sym, id=numgen(), round=payload(:round))
+end
--- routes.jl
+++ routes.jl
@@ -1,5 +1,18 @@
 using Genie.Router
+import CardgenController
 
 route("/") do
   serve_static_file("welcome.html")
 end
+
+route("/gencard/:round::Int/:id::Int/card.ps",
+      CardgenController.gencard_ps_r,
+      named=:get_card_ps)
+route("/gencard/:round::Int/:id::Int/card.pdf",
+      CardgenController.gencard_pdf_r,
+      named=:get_card_pdf)
+
+route("/gencard/:round::Int/seq.ps",
+      CardgenController.genseq(:get_card_ps))
+route("/gencard/:round::Int/seq.pdf",
+      CardgenController.genseq(:get_card_pdf))
